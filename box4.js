51ea3ab09b17563b6c78fca840535ed19d6f5195
Aadesh <70211331+AadeshTaneja@users.noreply.github.com>
2020-09-20 15:20:08 +0530

    Add files via upload

---
+++ box4.js
@@ -0,0 +1,30 @@
+class Box4{
+    constructor(x, y, width, height) {
+        var options = {
+            'restitution':0.8,
+            'friction':10,
+            'density':0.1,
+            'isStatic':false
+        }
+       
+
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+       
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        rectMode(CENTER);
+        fill(53,202,203)
+       
+        
+       
+       rect(0,0,this.width,this.height)
+        pop();
+      }
+}
